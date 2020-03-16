7254c18ae049dc359229311abf5cd357c9806169
Shaina C <scailles23@gmail.com>
2020-03-16 09:04:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+array=[]
+def oxford_comma(array)
+  return array.join if array.size==1
+  return array.join(" and ") if array.size==2
+  return array[0..-2].join(", ") + ", and "+ array[-1]
+end
