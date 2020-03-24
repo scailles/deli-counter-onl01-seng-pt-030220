f2ca80c0713b2629f5321a57d155a5713d9e7a8e
Shaina C <scailles23@gmail.com>
2020-03-24 21:45:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(line)
   if line.size==0
     puts "The line is currently empty."
   end
-  else line.each.with_map(1) do |person, index|
+  if line.each.with_map(1) do |person, index|
     current<<  "#{index}. #{person}."
   end
 end
