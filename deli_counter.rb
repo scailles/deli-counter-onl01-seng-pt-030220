3c05668a55226307a3a4f04b5d831a41791ce87b
Shaina C <scailles23@gmail.com>
2020-03-24 22:01:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(number, index)
   if number.size==0
 puts "There is nobody waiting to be served!"
 else number.size >=1
-  puts "Currently serving #{number.index(1)}."
+  puts "Currently serving #{number.index}."
   number.shift
 end
 end
