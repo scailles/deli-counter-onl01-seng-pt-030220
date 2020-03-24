6ad132a37ac94efb8f3410c677d5bb0d4ebafabe
Shaina C <scailles23@gmail.com>
2020-03-24 21:50:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ deli_line=[]
 
 def line(deli_line)
   current="The line is currently: "
-  if line.size==0
+  if deli_line.size==0
     puts "The line is currently empty."
   
   else deli_line.size >= 1
