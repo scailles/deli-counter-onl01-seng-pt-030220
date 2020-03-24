9f468a6646a25876255410517fc326e03278d668
Shaina C <scailles23@gmail.com>
2020-03-24 21:44:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(line)
   if line.size==0
     puts "The line is currently empty."
   else line.each.with_map(1) do |person, index|
-    puts "The line is currently: #{index}. #{person}."
+    current<<  "#{index}. #{person}."
     
     
   end
