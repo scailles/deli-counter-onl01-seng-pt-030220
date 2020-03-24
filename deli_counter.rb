cc1a11a1f00fa843f64fd003f8ff00c54878f345
Shaina C <scailles23@gmail.com>
2020-03-24 21:51:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli_line)
   
   else deli_line.size >= 1
     deli_line.each.with_index(1) do |person, index|
-    current<<  "#{index}. #{person}."
+    current<< "#{index}. #{person}."
   end
   current
 end
